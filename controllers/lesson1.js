26a9969bf4ee9511a2ced99349c32f689395f658
Madi <lut19003@byui.edu>
2023-09-15 17:58:48 -0600

    file reorginizing

---
+++ controllers/lesson1.js
@@ -0,0 +1,14 @@
+const tylerRoute = (req, res) => {
+    res.send("Tyler Blotter");
+};
+
+const madiRoute = (req, res) => {
+    res.send("Madi Blotter");
+};
+
+
+
+module.exports ={
+    tylerRoute,
+    madiRoute
+}
