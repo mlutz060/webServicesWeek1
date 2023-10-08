2214cae07ae5c54001e852af9839a48ac2c850ac
Madi <lut19003@byui.edu>
2023-10-07 19:34:29 -0600

    updated swagger and rest file

--- server.js
+++ server.js
@@ -1,12 +1,16 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const mongodb = require('./database/connect');
+const swaggerUi = require('swagger-ui-express');
+const swaggerDocument = require('./swagger/swagger.json');
+
 
 const port = process.env.PORT || 3000;
 const app = express();
 
 app
   .use(bodyParser.json())
+  .use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument))
   .use((req, res, next) => {
     res.setHeader('Access-Control-Allow-Origin', '*');
     next();
